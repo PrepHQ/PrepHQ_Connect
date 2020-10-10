357fab8939bee4f5637ced97a550e14bbae8b348
Sarah Oswald <SOswald@crimson.ua.edu>
2020-10-09 21:35:36 -0500

    Fixed alignment for "my account" and "logout" buttons on student and mentor screens
    Temp fix since we plan to move "logout" inside of the settings menu in the top bar, but workable for Sprint 1

--- lib/mentor/home.dart
+++ lib/mentor/home.dart
@@ -10,73 +10,81 @@ class MentorHome extends StatelessWidget {
     Widget build(BuildContext context) {
       return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children:[
-            Text("Welcome back, *Mentor*",
-            textScaleFactor: 2,
-            textAlign: TextAlign.justify,
-            ),
-            Text("This is your home page!",
-              textScaleFactor: 1,
-              textAlign: TextAlign.justify,
-            ),
-
-            RaisedButton(
-              child: Text(" Live Chat with Mentee "),
-              onPressed: tmpFunction,
-              color: Color(0xff33D2B0),
-              textColor: Colors.white,
-              splashColor: Colors.grey,
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            ),
-
-            RaisedButton(
-              child: Text(" Schedule "),
-              onPressed: tmpFunction,
-              color: Color(0xff22CDF7),
-              textColor: Colors.white,
-              splashColor: Colors.grey,
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            ),
-
-            RaisedButton(
-              child: Text(" List of Mentees "),
-              onPressed: tmpFunction,
-              color: Color(0xff096BD8),
-              textColor: Colors.white,
-              splashColor: Colors.grey,
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-            ),
-
-            new ButtonTheme.bar(
-              child: new ButtonBar(
-                alignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new RaisedButton(
-                    onPressed: tmpFunction, //Should direct to My Account Screen
-                    color: Color(0xff808786),
-                    textColor: Colors.white,
-                    splashColor: Colors.grey,
-                    child: Text(" My Account "),
+          body: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: new ButtonTheme.bar(
+                    child: new ButtonBar(
+                      alignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new RaisedButton(
+                          onPressed: tmpFunction, //Should direct to My Account Screen
+                          color: Color(0xff808786),
+                          textColor: Colors.white,
+                          splashColor: Colors.grey,
+                          child: Text(" My Account "),
+                        ),
+                        new RaisedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          color: Color(0xff808786),
+                          textColor: Colors.white,
+                          splashColor: Colors.grey,
+                          child: Text('Log Out'),
+                        ),
+                      ],
+                    ),
                   ),
+                ),
+                Center
+                  (
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children:[
+                      Text("Welcome back, *Mentor*",
+                        textScaleFactor: 2,
+                        textAlign: TextAlign.justify,
+                      ),
+                      Text("These are your tools!",
+                        textScaleFactor: 1,
+                        textAlign: TextAlign.justify,
+                      ),
 
-                  new RaisedButton(
-                    onPressed: () {
-                        Navigator.pop(context);
-                    },
-                    color: Color(0xff808786),
-                    textColor: Colors.white,
-                    splashColor: Colors.grey,
-                    child: Text('Log Out'),
+                      RaisedButton(
+                        child: Text(" Live Chat with Mentee "),
+                        onPressed: tmpFunction,
+                        color: Color(0xff33D2B0),
+                        textColor: Colors.white,
+                        splashColor: Colors.grey,
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                      ),
+
+                      RaisedButton(
+                        child: Text(" Schedule "),
+                        onPressed: tmpFunction,
+                        color: Color(0xff22CDF7),
+                        textColor: Colors.white,
+                        splashColor: Colors.grey,
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                      ),
+
+                      RaisedButton(
+                        child: Text(" List of Mentees "),
+                        onPressed: tmpFunction,
+                        color: Color(0xff096BD8),
+                        textColor: Colors.white,
+                        splashColor: Colors.grey,
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+
+                      ),
+
+                    ],
                   ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      )
-    );
-  }
+                )
+              ]
+          )
+      );
+    }
 }
