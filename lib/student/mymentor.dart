9aecff5182b9818eacb3cd3780c7e248978d3971
Caleb Werth <cwerth@crimson.ua.edu>
2020-10-29 16:06:36 -0500

    first draft of mentor selection screen

--- lib/student/mymentor.dart
+++ lib/student/mymentor.dart
@@ -5,8 +5,86 @@ class MyMentorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("My Mentor", textScaleFactor: 2),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            // TODO: write function to generate these rows/cards
+            children: [
+              Row(
+                children: [
+                  MentorProfileCard(),
+                  MentorProfileCard(),
+                ],
+              ),
+              Row(
+                children: [
+                  MentorProfileCard(),
+                  MentorProfileCard(),
+                ],
+              ),
+              Row(
+                children: [
+                  MentorProfileCard(),
+                  MentorProfileCard(),
+                ],
+              ),
+              Row(
+                children: [
+                  MentorProfileCard(),
+                  MentorProfileCard(),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MentorProfileCard extends StatelessWidget {
+  MentorProfileCard({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        child: InkWell(
+          splashColor: Color.fromRGBO(75, 209, 160, 1).withAlpha(30),
+          onTap: () {
+            print('Card tapped.');
+          },
+          child: Container(
+            height: 250,
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                CircleAvatar(
+                  radius: 65,
+                  backgroundImage: AssetImage('images/caleb_headshot.jpg'),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      //TODO: Make this not hardcoded.
+                      Text('Caleb Werth', style: TextStyle(
+                          fontFamily: 'Montserrat', fontWeight: FontWeight.bold, fontSize: 17.0
+                      ),),
+                      Text('The University of Alabama', style: TextStyle(
+                          fontFamily: 'Montserrat', fontStyle: FontStyle.italic, fontSize: 14.0
+                      ),),
+                      Text('Senior - Computer Science', style: TextStyle(
+                        fontFamily: 'Montserrat', fontSize: 13.0,
+                      ),),
+                    ],
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
