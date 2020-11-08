9708bae8b7e50c7ab56d9455f21ae40ea82e607a
Caleb Werth <cwerth@crimson.ua.edu>
2020-11-07 21:41:57 -0600

    Added more test cases

--- test/general_ui_test.dart
+++ test/general_ui_test.dart
@@ -4,13 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:mockito/mockito.dart';
 import 'package:prephq_connect/views/auth/login.dart';
 import 'package:prephq_connect/registrationscreen.dart';
+import 'package:prephq_connect/views/mentor/home.dart';
+import 'package:prephq_connect/views/mentor/mentorscreen.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
-  testWidgets('Validate login screen widgets initialize correctly', (WidgetTester tester) async {
+  testWidgets('Validate login screen widgets initialize correctly',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(home: LoginPage()));
+
+    expect(find.byType(TextFormField), findsWidgets);
+    expect(find.byType(MaterialButton), findsOneWidget);
 
     // Verify our hint text
     expect(find.text('Email'), findsOneWidget);
@@ -18,7 +25,8 @@ void main() {
     expect(find.text('Not_There'), findsNothing);
   });
 
-  testWidgets('Validate registration screen widgets initialize correctly', (WidgetTester tester) async {
+  testWidgets('Validate registration screen widgets initialize correctly',
+      (WidgetTester tester) async {
     // Build registration form
     await tester.pumpWidget(MaterialApp(home: RegistrationForm()));
 
@@ -32,7 +40,8 @@ void main() {
     expect(find.text('Not_There'), findsNothing);
   });
 
-  testWidgets('Validate navigation to registration page from login screen works',
+  testWidgets(
+      'Validate navigation to registration page from login screen works',
       (WidgetTester tester) async {
     final mockObserver = MockNavigatorObserver();
     await tester.pumpWidget(
@@ -55,4 +64,26 @@ void main() {
     expect(find.byType(RegistrationForm), findsOneWidget);
   });
 
+  testWidgets(
+    'Validate that Mentor Home widgets are initialized',
+    (WidgetTester tester) async {
+      mockNetworkImagesFor(() async {
+        await tester.pumpWidget(MaterialApp(home: MentorHome()));
+
+        expect(find.byType(RaisedButton), findsWidgets);
+        expect(find.byType(Text), findsWidgets);
+      });
+    },
+  );
+
+  testWidgets(
+    'Validate that Mentor Screen bottom app bar navigates to MentorHome by default as expected',
+        (WidgetTester tester) async {
+      mockNetworkImagesFor(() async {
+        await tester.pumpWidget(MaterialApp(home: MentorScreen()));
+
+        expect(find.byType(MentorHome), findsOneWidget);
+      });
+    },
+  );
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,25 +1,38 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:prephq_connect/widgets/appbar_action_btn.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+import 'package:prephq_connect/widgets/article_card.dart';
 
 void main() {
   testWidgets('Validate AppBarActionBtn constructs as expected',
-          (WidgetTester tester) async {
-        // Build our app and trigger a frame.
-        await tester.pumpWidget(MaterialApp(
-          home: Scaffold(
-              appBar: AppBar(
-                actions: [
-                  AppBarActionBtn(
-                    icon: Icons.play_arrow,
-                  )
-                ],
-              )),
-        ));
+      (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+        actions: [
+          AppBarActionBtn(
+            icon: Icons.play_arrow,
+          )
+        ],
+      )),
+    ));
 
-        expect(find.byType(AppBarActionBtn), findsOneWidget);
-        expect(find.byIcon(Icons.play_arrow), findsOneWidget);
-        expect(find.byIcon(Icons.ac_unit), findsNothing);
+    expect(find.byType(AppBarActionBtn), findsOneWidget);
+    expect(find.byIcon(Icons.play_arrow), findsOneWidget);
+    expect(find.byIcon(Icons.ac_unit), findsNothing);
+  });
+
+  testWidgets(
+    'Test that article card constructs as expected',
+    (WidgetTester tester) async {
+      mockNetworkImagesFor(() async {
+        await tester.pumpWidget(MaterialApp(home: ArticleCard()));
+        expect(find.byType(Text), findsNWidgets(2));
+        expect(find.byType(FlatButton), findsOneWidget);
+        expect(find.byType(Divider), findsOneWidget);
       });
-
+    },
+  );
 }
