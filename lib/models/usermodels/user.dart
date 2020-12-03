48e1f52df98efe59ed0a8f9697f4e5f436397b3f
IcemanOmikron <stvnengineer@gmail.com>
2020-12-02 22:26:38 -0600

    Globals for chat.

--- lib/models/usermodels/user.dart
+++ lib/models/usermodels/user.dart
@@ -1,6 +1,8 @@
 String id = "";
 String email = "";
 String imageURL = "https://firebasestorage.googleapis.com/v0/b/prephq-connect.appspot.com/o/profilePictures%2FnoPic.png?alt=media&token=0b5e58c4-9999-4245-b9b7-6438e23f3020";
+String name = "";
+
 
 enum UserTypes { Student, Mentor }
 
